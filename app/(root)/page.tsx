fef21ab3ef7a79816857804a51dbebacf41d618c
jacobogonzalezdiaz1974 <jacobogonzalez1974@outlook.com>
2024-11-16 20:19:10 +0000

    implement auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
 import HeaderBox from '@/components/HeaderBox'
 import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
+import { getLoggedInUser } from '@/lib/actions/user.action'
 import React from 'react'
 
-const Home = () => {
-  const loggedIn = {firstname: "Jacobo", lastName: "Gonzalez", email: "jgonzalez@example.com" }
+const Home = async () => {
+  const loggedIn = await getLoggedInUser()
   return (
     <section className="home">
       <div className="home-content">
@@ -12,7 +13,7 @@ const Home = () => {
           <HeaderBox 
           type='greeting' 
           title='Welcome' 
-          user={loggedIn?.firstname || "Jacobo"} 
+          user={loggedIn?.name || "Jacobo"} 
           subtext='Access and manage your account and transactions efficiently' />
           <TotalBalanceBox 
           accounts={[]}
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -6,9 +6,10 @@ import { cookies } from "next/headers";
 export async function createSessionClient() {
   const client = new Client()
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
+    .setKey(process.env.NEXT_APPWRITE_KEY!);
 
-  const session = cookies().get("appwrite-session");
+  const session = (await cookies()).get("appwrite-session");
 
   if (!session || !session.value) {
     throw new Error("No session");
